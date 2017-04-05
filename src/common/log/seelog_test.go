69b55801d465a591d2b39b6bc9b41db8e22ee92c
wsp_work <wsp_work@163.com>
2017-04-05 19:26:45 +0800

    add /common/log/seelog.go、seelog_test.log and seelog.xml

---
+++ src/common/log/seelog_test.go
@@ -0,0 +1,78 @@
+package log
+
+import (
+	"testing"
+	"fmt"
+	"time"
+)
+
+func TestTrace(t *testing.T) {
+	Trace("trace test....")
+}
+
+func TestTracef(t *testing.T) {
+	Tracef("测试%s","trace")
+}
+
+
+func TestDebuge(t *testing.T) {
+	Debuge("debuge test....")
+}
+
+func TestDebugef(t *testing.T) {
+	Debugef("测试%s","debuge")
+}
+
+
+func TestInfo(t *testing.T) {
+	for ; ; {
+		Info("Info test....")
+		Debuge("debuge test....")
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func TestInfof(t *testing.T) {
+	Infof("测试%s","Infof")
+}
+
+
+func TestWarn(t *testing.T) {
+	Warn("Warn test....")
+}
+
+func TestWarnf(t *testing.T) {
+	Warnf("测试%s","Warnf")
+}
+
+
+func TestError(t *testing.T) {
+	Error("Error test....")
+}
+
+func TestErrorf(t *testing.T) {
+	Errorf("测试%s","Errorf")
+}
+
+
+func TestCritical(t *testing.T) {
+	Critical("Critical test....")
+}
+
+func TestCriticalf(t *testing.T) {
+	Criticalf("测试%s","Criticalf")
+}
+
+func TestClosed(t *testing.T) {
+	Info("begin")
+	fmt.Println("关闭了吗?",Closed())
+	Close()
+	fmt.Println("关闭了吗?",Closed())
+	Info("end")
+}
+
+func TestClose(t *testing.T) {
+	Info("SSSS")
+	Close()
+	Info("CCCC")
+}
