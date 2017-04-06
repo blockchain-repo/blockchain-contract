c560aa5aa59e3be36e0b703c390857feb44f2fa1
KURO1 <412681778@qq.com>
2017-04-06 16:43:36 +0800

    rethinkdb:changefeed

--- src/core/db/rethinkdb/changefeed.go
+++ src/core/db/rethinkdb/changefeed.go
@@ -1,1 +1,16 @@
 package rethinkdb
+
+import (
+	"log"
+
+	r "gopkg.in/gorethink/gorethink.v3"
+)
+
+func Changefeed(db string, name string) *r.Cursor {
+	session := ConnectDB(db)
+	res, err := r.Table(name).Changes().Run(session)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
+}
---
+++ src/core/db/rethinkdb/changefeed_test.go
@@ -0,0 +1,14 @@
+package rethinkdb
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_Changefeed(t *testing.T) {
+	var value interface{}
+	res :=Changefeed("bigchain","backlog")
+	for res.Next(&value) {
+		fmt.Println(value)
+	}
+}
