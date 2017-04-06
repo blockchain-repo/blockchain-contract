7e7cb6064cd00ff273ad531b79d3a9ddd04e6abd
wsp_work <wsp_work@163.com>
2017-04-06 19:40:57 +0800

    modify install_protoc.sh and add install_nsq.sh

--- src/common/protoc3/install_protoc.sh
+++ src/common/protoc3/install_protoc.sh
@@ -2,14 +2,16 @@
 set -e
 #下载protoc3
 wget https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip -c -P /opt
-mkdir /opt/protoc
 
+if [! -d "/opt/protoc" ];then
+    mkdir /opt/protoc
+fi
 #下载zip
-apt-get install zip
+apt-get -y install zip
 
 #解压zip包
 unzip /opt/protoc-3.0.0-linux-x86_64.zip -d /opt/protoc
-chomd +x /opt/protoc/
+chmod +x /opt/protoc/bin/protoc
 
 #配置环境变量
 echo export PATH=$PATH:/opt/protoc/bin >>/etc/profile
