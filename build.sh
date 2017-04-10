eb0108de0703db0348d8d91c698c3e77d3b16bf0
wangxin <wx_cs_db_88@163.com>
2017-04-10 08:42:00 +0800

    fixed the project GOPATH and govendor

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-export GOPATH=/home/wangxin/company/golang/src/unicontract:$GOPATH
+# The set -e option instructs bash to immediately exit
+# if any command has a non-zero exit status
+set -e
+
+CUR_PATH=$(cd "$(dirname "$0")"; pwd)
+export GOPATH=${CUR_PATH}:$GOPATH
 
 #bee version -o json
+
+#go get -u github.com/kardianos/govendor
+go get github.com/kardianos/govendor
 
 #govendor init
 govendor init
