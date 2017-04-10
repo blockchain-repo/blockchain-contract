cb7de3070e6c2eccf506db41fbccc7f0016359be
wangxin <wx_cs_db_88@163.com>
2017-04-10 20:32:59 +0800

    fixed the proto generate method

--- proto-gen-go.sh
+++ proto-gen-go.sh
@@ -3,11 +3,15 @@
 # if any command has a non-zero exit status
 set -e
 
-protos_file_path=src/core/protos/
+CUR_PATH=$(cd "$(dirname "$0")"; pwd)
+
+protos_file_path=${CUR_PATH}/src/core/protos/
 
 export PATH=/opt/protoc/bin:$PATH
 
-if [ -f ${protos_file_path}/*.proto ]; then
+proto_count=`find ${protos_file_path} -name *.proto|wc -l`
+
+if [ "${proto_count}" != "0" ] ; then
     echo -e "\033[01;34m reproduce the *.go files according the *.proto files \033[1m"
     protoc -I=src/core/protos --go_out=src/core/protos/ src/core/protos/*.proto
     echo -e "\033[01;34m reproduce success! \033[1m"
