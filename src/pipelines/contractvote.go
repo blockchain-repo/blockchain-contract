fde1c3acc59e0e07b6f8e24aab7e9e02fb7fd264
KURO1 <412681778@qq.com>
2017-04-14 09:39:12 +0800

    fix contractvote

--- src/pipelines/contractvote.go
+++ src/pipelines/contractvote.go
@@ -1,19 +1,18 @@
 package pipelines
 
 import (
-//	"time"
-//	"fmt"
-	"os"
-	"io"
-	"log"
+	//	"time"
+	//	"fmt"
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"io"
+	"log"
+	"os"
 	r "unicontract/src/core/db/rethinkdb"
 )
 
-
-func cvchangefeed(in io.Reader, out io.Writer) {
+func cvChangefeed(in io.Reader, out io.Writer) {
 	var value interface{}
 	res := r.Changefeed("Unicontract", "Contract")
 	for res.Next(&value) {
@@ -27,61 +26,60 @@ func cvchangefeed(in io.Reader, out io.Writer) {
 }
 
 //TODO: core validate func
-func validateContract(in io.Reader, out io.Writer) {
-    rd := bufio.NewReader(in)
-    p := make([]byte,MaxSizeTX)
-    for {
-        n, _ := rd.Read(p)
-        if n == 0 {
-            break
-        }
-        t := bytes.ToUpper(p[:n])
-        out.Write(t)
-    }
+func cvValidateContract(in io.Reader, out io.Writer) {
+	rd := bufio.NewReader(in)
+	p := make([]byte, MaxSizeTX)
+	for {
+		n, _ := rd.Read(p)
+		if n == 0 {
+			break
+		}
+		t := bytes.ToUpper(p[:n])
+		out.Write(t)
+	}
 }
 
 //TODO: core make vote
-func vote(in io.Reader, out io.Writer) {
-    rd := bufio.NewReader(in)
-    p := make([]byte,MaxSizeTX)
-    for {
-        n, _ := rd.Read(p)
-        if n == 0 {
-            break
-        }
-        t := bytes.ToLower(p[:n])
-        out.Write(t)
-    }
+func cvVote(in io.Reader, out io.Writer) {
+	rd := bufio.NewReader(in)
+	p := make([]byte, MaxSizeTX)
+	for {
+		n, _ := rd.Read(p)
+		if n == 0 {
+			break
+		}
+		t := bytes.ToLower(p[:n])
+		out.Write(t)
+	}
 }
 
-
 //TODO:core write vote ??? UPDATE
-func writeVote(in io.Reader, out io.Writer) {
-    rd := bufio.NewReader(in)
-    p := make([]byte,MaxSizeTX)
-    for {
-        n, _ := rd.Read(p)
-        if n == 0 {
-            break
-        }
-        t := p[:n]
-	r.Insert("Unicontract","Votes",string(t))
-	out.Write(t)
-    }
+func cvWriteVote(in io.Reader, out io.Writer) {
+	rd := bufio.NewReader(in)
+	p := make([]byte, MaxSizeTX)
+	for {
+		n, _ := rd.Read(p)
+		if n == 0 {
+			break
+		}
+		t := p[:n]
+		r.Insert("Unicontract", "Votes", string(t))
+		out.Write(t)
+	}
 }
 
 func startContractVote() {
 
-	p:=Pipe(
-	cvchangefeed,
-	validateContract,
-	vote,
-	writeVote)
+	p := Pipe(
+		cvChangefeed,
+		cvValidateContract,
+		cvVote,
+		cvWriteVote)
 
 	f, err := os.OpenFile("/dev/null", os.O_RDWR, 0)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 	w := bufio.NewWriter(f)
-	p(nil,w)
+	p(nil, w)
 }
