c9f34244021f8cfb189c843c8b603cef47779e0a
wangxin <wx_cs_db_88@163.com>
2017-04-19 09:38:09 +0800

    change the routers for api and remove the commentRouters

--- src/api/routers/router.go
+++ src/api/routers/router.go
@@ -10,61 +10,22 @@ package routers
 import (
 	"github.com/astaxie/beego"
 	"unicontract/src/api/controllers"
-	"net/http"
-	"fmt"
-	//"unicontract-back/src/core/model"
-	"github.com/astaxie/beego/context"
 	"unicontract/src/api/filters"
-	//"unicontract-back/src/core/model"
 )
-
-func msgHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, HandlerFunc!")
-	fmt.Println("heel")
-	return
-}
-
-var UrlManager = func(ctx *context.Context) {
-	// 数据库读取全部的 url mapping 数据
-	fmt.Println("heel")
-	fmt.Println("heel")
-	fmt.Println("heel")
-	types := ctx.Input.Header("ReqData-Type")
-	Content_Type := ctx.Input.Header("Content-Type")
-	fmt.Println("types is ",types)
-	fmt.Println("Content_Type is ",Content_Type)
-	if types == "proto"{
-		fmt.Println("need use 数据")
-	}else if types == "json"{
-		//ctx.Abort(401,"222")
-		ctx.Output.SetStatus(404)
-		ctx.ResponseWriter.Write([]byte("gogogo back"))
-		fmt.Println("need use 数据")
-	}else{
-		return
-	}
-}
 
 func init() {
 
-	//beego.Handler("/v1/contract/*", http.HandlerFunc(msgHandler))
-	//beego.NSHandler("/v1/contract/", http.HandlerFunc(msgHandler))
-	beego.InsertFilter("/*",beego.BeforeRouter, filters.ContractFilter, false)
+	beego.InsertFilter("/*", beego.BeforeRouter, filters.ContractFilter, false)
 	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
 		beego.NSNamespace("/contract",
-			beego.NSInclude(
-				&controllers.ContractController{},
-			),
+			beego.NSRouter("/authSignature", &controllers.ContractController{}, "post:AuthSignature"),
+			beego.NSRouter("/create", &controllers.ContractController{}, "post:Create"),
+			beego.NSRouter("/signature", &controllers.ContractController{}, "post:Signature"),
+			beego.NSRouter("/terminate", &controllers.ContractController{}, "post:Terminate"),
+			beego.NSRouter("/find", &controllers.ContractController{}, "post:Find"),
+			beego.NSRouter("/track", &controllers.ContractController{}, "post:Track"),
+			beego.NSRouter("/update", &controllers.ContractController{}, "post:Update"),
+			beego.NSRouter("/test", &controllers.ContractController{}, "post:Test"),
 		),
 	)
 	beego.AddNamespace(ns)
