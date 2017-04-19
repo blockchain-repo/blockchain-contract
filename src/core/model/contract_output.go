bb7d2558cd04755474a47f2b2a740716117c73e2
wangxin <wx_cs_db_88@163.com>
2017-04-19 19:29:41 +0800

    added methods for coModel [HasEnoughVotes, ValidteHash]

--- src/core/model/contract_output.go
+++ src/core/model/contract_output.go
@@ -42,3 +42,65 @@ func (c *ContractOutput) GetId() string {
 
 	return common.HashData(contract_without_signatures_serialized)
 }
+
+// judge has enough votes for ContractOutput
+func (c *ContractOutput) HasEnoughVotes() bool {
+	voters := c.Transaction.Relaction.Voters
+	signatures := c.Transaction.Relaction.Signatures
+	voters_len := len(voters)
+
+	if voters_len <= 0 {
+		return false
+	}
+
+	if len(signatures) != voters_len {
+		return false
+	}
+
+	var invalid_signature_len int
+	for index, voter := range voters {
+		if voter == "" {
+			return false
+		}
+
+		_signature := signatures[index]
+		contract_node_pubkey := _signature.ContractNodePubkey
+		signature := _signature.Signature
+		_ = signature
+
+		if contract_node_pubkey == "" {
+			invalid_signature_len += 1
+		} else {
+			//todo choice 1:  非法投票是否立即报错！
+			if voter != contract_node_pubkey || signature == "" {
+				return false
+			}
+
+			//todo choice 2: 非法投票不计算
+			//if voter != contract_node_pubkey || signature == "" {
+			//	invalid_signature_len += 1
+			//}
+		}
+
+	}
+
+	if invalid_signature_len >= voters_len/2 {
+		return false
+	}
+	return true
+}
+
+// 判断hash，hash不包括voters的signatures
+func (c *ContractOutput) ValidHash(id string) bool {
+	hashId := id
+	rightId := c.GetId()
+	if hashId != rightId {
+		return false
+	}
+	return true
+}
+
+//  判断是否有>1/2的有效签名。 return bool
+func (c *ContractOutput) ValidSignature() bool {
+	return true
+}
