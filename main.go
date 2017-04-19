52a974eddbd95a28227a07ebe9b2662d1c2d249d
wangxin <wx_cs_db_88@163.com>
2017-04-19 10:12:07 +0800

    if u wanot output the logs to console, you can open the comment line in main.go

--- main.go
+++ main.go
@@ -11,39 +11,25 @@ import (
 func main() {
 	beego.LoadAppConfig("ini", "../conf/app.conf")
 
+	//日志默认不输出调用的文件名和文件行号,如果你期望输出调用的文件名和文件行号,可以如下设置
 	//logs.SetLogFuncCall(true)
+	//如果你的应用自己封装了调用 log 包,那么需要设置 SetLogFuncCallDepth,默认是 2,
+	// 也就是直接调用的层级,如果你封装了多层,那么需要根据自己的需求进行调整.
 	//logs.EnableFuncCallDepth(true)
+
+	//如果不想在控制台输出log相关的，可以打开下面设置
+	//todo if u want not output to console, open following line!
+	//beego.BeeLogger.DelLogger("console")
 
 	logs.SetLogger(logs.AdapterMultiFile, `{"filename":"unicontract.log","level":7,
 	"maxlines":0,"maxsize":0,"daily":true,"maxdays":10,
 	"separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
 	logs.Warn("main start")
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
 
-	beego.Run()
-
-	//var command *string = flag.String("c", "command", "Start beego")
-	//flag.Parse()
-	//fmt.Println("param command is \t", *command)
-	//if command != nil {
-	//
-	//	logs.SetLogger(logs.AdapterFile, `{"filename":"project.log","level":7,
-	//"maxlines":0,"maxsize":0,"daily":true,"maxdays":10}`)
-	//	logs.Warn("main start")
-	//	if *command == "api" {
-	//		if beego.BConfig.RunMode == "dev" {
-	//			beego.BConfig.WebConfig.DirectoryIndex = true
-	//			beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	//		}
-	//
-	//		beego.Run()
-	//	} else if *command == "test" {
-	//		fmt.Println("test...")
-	//	}
-
+	//if beego.BConfig.RunMode == "dev" {
+	//	beego.BConfig.WebConfig.DirectoryIndex = true
+	//	beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	//}
 
+	beego.Run()
 }
