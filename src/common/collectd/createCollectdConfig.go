243b2f7197e6ecd2fa4b41e4fc29a2ec3f576654
wsp_work <wsp_work@163.com>
2017-04-20 11:54:29 +0800

    add createCollectdConfig.go and collectdConfig.conf.template

--- src/common/collectd/createCollectdConfig.go
+++ src/common/collectd/createCollectdConfig.go
@@ -5,6 +5,7 @@ import (
 	"github.com/astaxie/beego"
 	"unicontract/src/common/requestHandler"
 	"unicontract/src/common"
+	"io"
 )
 
 /**
@@ -13,40 +14,103 @@ import (
  * return : 
  */
 func CreateCollectdConfig(){
-
 	//获取模板配置文件
 	configPath := os.Getenv("CONFIGPATH")
-	templatePath := configPath + "/collectd.conf.template"
-	src,err := os.Open(templatePath)
-	if err != nil{
-		beego.Error(err.Error())
-	}
-	defer src.Close()
-
-	//创建配置文件,进行复制
-	configPath = configPath + "/collectd.conf"
-	collectdConf,err := os.OpenFile(configPath,os.O_WRONLY,0644)
-	if err != nil{
-		beego.Error(err.Error())
-	}
-	n,err := collectdConf.Seek(0,os.SEEK_END)
-	if err != nil{
-		beego.Error(err.Error())
-	}
-
+	templateConfig :=_GetTemplateConfig(configPath)
+	//删除旧的配置文件
+	_DeletOldConfigFile(configPath)
+	//创建新的配置文件
+	collectdConf := _CreatNeWCollectdConf(configPath,templateConfig)
+	defer templateConfig.Close()
 	//获取追加内容
+	netPlugin := _GetContent()
+	//文件内容追加
+	_AddContentToConf(collectdConf,netPlugin)
+}
+
+/**
+ * function : 获取模板配置文件
+ * param   :
+ * return : *os.File
+ */
+func _GetTemplateConfig(configPath string) *os.File{
+
+	templatePath := configPath + "/collectd.conf.template"
+	templateConfig,err := os.Open(templatePath)
+	if err != nil{
+		beego.Error(err.Error())
+	}
+	return templateConfig
+}
+
+/**
+ * function : 删除旧的配置文件
+ * param   :
+ * return : *os.File
+ */
+func _DeletOldConfigFile(configPath string) {
+
+	configFile := configPath + "/collectd.conf"
+	_,err := os.Stat(configFile)
+	if err != nil{
+		beego.Error("create new collectd.conf")
+	}
+	//文件存在则删除
+	if !os.IsNotExist(err){
+		err := os.Remove(configFile)
+		beego.Debug("delete old collectd.conf")
+		if err != nil{
+			beego.Error(err.Error())
+		}
+	}
+}
+
+/**
+ * function : 创建新的配置文件
+ * param   :
+ * return : *os.File
+ */
+func _CreatNeWCollectdConf(configPath string,templateConfig *os.File) *os.File{
+	configFile := configPath + "/collectd.conf"
+	collectdConf,err := os.Create(configFile)
+	if err != nil{
+		beego.Error(err.Error())
+	}
+	io.Copy(collectdConf,templateConfig)
+	return collectdConf
+}
+
+/**
+ * function : 获取追加内容
+ * param   :
+ * return : string
+ */
+func _GetContent() string{
 	config := requestHandler.GetYamlConfig("monitorConfig.yaml")
 	ip :=common.TypeToString(config["ip"])
 
 	netPlugin :=  `
-		<Plugin network>
-    		Server "` + ip + `" "25826"
-		</Plugin>
-	`
-	c, err := collectdConf.WriteAt([]byte(netPlugin),n)
-	beego.Debug(c)
+<Plugin network>
+    Server "` + ip + `" "25826"
+</Plugin>
+`
+	return netPlugin
+}
+
+/**
+ * function : 文件内容追加
+ * param   :
+ * return : string
+ */
+func _AddContentToConf(collectdConf *os.File,netPlugin string){
+	n,err := collectdConf.Seek(0,os.SEEK_END)
 	if err != nil{
 		beego.Error(err.Error())
 	}
-	defer  collectdConf.Close()
+	c, err := collectdConf.WriteAt([]byte(netPlugin),n)
+	if err != nil{
+		beego.Error(err.Error())
+	}
+	beego.Debug("create collectd.conf success...",c)
 }
+
---
+++ src/common/collectd/createCollectdConfig_test.go
@@ -0,0 +1,12 @@
+package collectd
+
+import "testing"
+
+/**
+ * function : 
+ * param   :
+ * return : 
+ */
+func TestCreateCollectdConfig(t *testing.T) {
+	CreateCollectdConfig()
+}
