308f6238bd4fb515de47b4730d2d0e66c481c90d
lizhen <294474470@qq.com>
2017-04-24 03:42:10 -0700

    update test file

--- src/pipelines/txelection_test.go
+++ src/pipelines/txelection_test.go
@@ -2,21 +2,8 @@ package pipelines
 
 import (
 	"testing"
-	"fmt"
 )
 
 func Test_pipStart(t *testing.T) {
-	//starttxElection()
-	//str1 := "Go"
-	//str2 := "go"
-	//fmt.Println(strings.EqualFold(str1,str2))
-	//fmt.Print(str1==str2)
-	validSignCount := 5
-	voters_len := 9
-	fmt.Print(voters_len/2 )
-	if validSignCount <= voters_len/2 {
-		fmt.Print("1")
-	}
-	fmt.Print("2")
-
+	starttxElection()
 }
