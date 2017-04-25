934051fa7d491c7efa8d1d038f348ada2b9db865
kuro1 <412681778@qq.com>
2017-04-25 14:00:10 +0800

    &protos.ContractHead{}

--- src/core/core_test.go
+++ src/core/core_test.go
@@ -4,24 +4,20 @@ import (
 	"testing"
 	"unicontract/src/common"
 	"unicontract/src/core/model"
+	"unicontract/src/core/protos"
 )
 
 func TestWriteContract(t *testing.T) {
 	contractModel := model.ContractModel{}
-	contractModel.Timestamp = common.GenTimestamp()
 //	private_key := "5Pv7F7g9BvNDEMdb8HV5aLHpNTNkxVpNqnLTQ58Z5heC"
 	// modify and set value for reference obj with &
-	contract := &contractModel.Contract
-	contract.CreatorPubkey = "5Pv7F7g9BvNDEMdb8HV5aLHpNTNkxVpNqnLTQ58Z5heC"
-	contract.Operation = "CREATE"
-	contract.ContractOwners = []string{
-		"2kdD14DHpccekjRgK55bgzEuAF5JLubhq3tBRm1sXqDc",
-		"2kdD14DHpccekjRgK55bgzEuAF5JLubhq3tBRm1sXqDc",
-		"JBMja2vDAJxkj9bxxjGzxQpTtavLxajxij41geufRXzs",
-		//"EtQVTBXJ8onJmXLnkzGBhbxhE3bSPgqvCkeaKtT22Cet",
+	contractBody := &protos.ContractBody{}
+	contractBody.ContractOwners = []string{
+		"qC5zpgJBqUdqi3Gd6ENfGzc5ZM9wrmqmiPX37M9gjq3",
+		"J2rSKoCuoZE1MKkXGAvETp757ZuARveRvJYAzJxqEjoo",
+		"EtQVTBXJ8onJmXLnkzGBhbxhE3bSPgqvCkeaKtT22Cet",
 	}
-	// sign for contract
-
+	contractModel.ContractBody = contractBody
 	contractModel.Id = common.GenTimestamp()
 	WriteContract(contractModel)
 }
