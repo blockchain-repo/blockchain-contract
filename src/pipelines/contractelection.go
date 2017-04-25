346d8a255dec213b38f806e27111545def823a07
wangyp@chainen.net <wangyp@chainen.net>
2017-04-25 17:53:19 +0800

    modify log

--- src/pipelines/contractelection.go
+++ src/pipelines/contractelection.go
@@ -27,12 +27,14 @@ import (
 
 //---------------------------------------------------------------------------
 const (
-	_DBName         = "Unicontract"
-	_TableNameVotes = "Votes"
-	_NewVal         = "new_val"
-	_HTTPOK         = 200
-	_VERSION        = 2
-	_OPERATION      = "CONTRACT"
+	_DBName          = "Unicontract"
+	_TableNameVotes  = "Votes"
+	_NewVal          = "new_val"
+	_HTTPOK          = 200
+	_VERSION         = 2
+	_OPERATION       = "CONTRACT"
+	_CONSENSUSTYPE   = "contract"
+	_CONSENSUSREASON = "contract illegal"
 )
 
 var (
@@ -43,8 +45,6 @@ var (
 
 //---------------------------------------------------------------------------
 func init() {
-	log.SetPrefix("---------")
-
 	gslPublicKeys = config.GetAllPublicKey()
 	gnPublicKeysNum = len(gslPublicKeys)
 	gstrPublicKey = config.Config.Keypair.PublicKey
@@ -52,19 +52,15 @@ func init() {
 
 //---------------------------------------------------------------------------
 func ceChangefeed(in io.Reader, out io.Writer) {
-	log.Printf("1.进入ceChangefeed\n")
+	beegoLog.Debug("1.进入ceChangefeed")
 	var value interface{}
 	res := rethinkdb.Changefeed(_DBName, _TableNameVotes)
 	for res.Next(&value) {
 		time := monitor.Monitor.NewTiming()
-
+		beegoLog.Debug("1.1 ceChangefeed get new_val")
 		mValue := value.(map[string]interface{})
-		//log.Printf("5.mValue is %+v\n", mValue)
-		log.Println("5")
 		// 提取new_val的值
 		slVote, err := json.Marshal(mValue[_NewVal])
-		//log.Printf("6.mValue[_NewVal] is %+v\n", mValue[_NewVal])
-		log.Println("6")
 		if err != nil {
 			beegoLog.Error(err.Error())
 			continue
@@ -73,22 +69,23 @@ func ceChangefeed(in io.Reader, out io.Writer) {
 			beegoLog.Error("is null")
 			continue
 		}
-		log.Printf("7.ceChangefeed--->out.Write(slVote)\n")
-		out.Write(slVote)
 
+		beegoLog.Debug("1.2 ceChangefeed --->")
+		out.Write(slVote)
 		time.Send("votes_changefeed")
 	}
 }
 
 //---------------------------------------------------------------------------
 func ceHeadFilter(in io.Reader, out io.Writer) {
-	log.Printf("2.进入ceHeadFilter\n")
+	beegoLog.Debug("2.进入ceHeadFilter")
 	rd := bufio.NewReader(in)
 	slVote := make([]byte, MaxSizeTX)
 	for {
 		// 读取new_val的值
 		nReadNum, err := rd.Read(slVote)
 		time := monitor.Monitor.NewTiming()
+		beegoLog.Debug("2.1 ceHeadFilter get data")
 		if err != nil {
 			beegoLog.Error(err.Error())
 			continue
@@ -105,18 +102,15 @@ func ceHeadFilter(in io.Reader, out io.Writer) {
 			beegoLog.Error(err.Error())
 			continue
 		}
-		//log.Printf("8.vote is %+v\n", vote)
-		log.Println("8")
 
 		// 验证是否为头节点
+		beegoLog.Debug("2.2 get publickey and verify")
+		beegoLog.Debug("2.2.1 get publickey")
 		mainPubkey, err := rethinkdb.GetContractMainPubkeyByContract(vote.VoteBody.VoteFor)
-		//log.Printf("9.mainPubkey is %+v\n", mainPubkey)
-		log.Println("9")
 		if err == nil {
+			beegoLog.Debug("2.2.2 verify head node")
 			if isHead, _ := _verifyHeadNode(mainPubkey); isHead {
-				//log.Printf("11.isHead is %+v\n", isHead)
-				log.Println("11")
-
+				beegoLog.Debug("2.2.3 verify vote")
 				slMyContract, pass, err := _verifyVotes(vote.VoteBody.VoteFor)
 				if err != nil {
 					if !pass { // 只有产生错误时才记录日志，当vote数量不够节点数量的一半时直接进入下次等待，不记录错误
@@ -126,15 +120,15 @@ func ceHeadFilter(in io.Reader, out io.Writer) {
 				}
 
 				if pass { // 合约合法
-					log.Printf("18.slMyContract is %+v\n", string(slMyContract))
-					//log.Println("18")
+					beegoLog.Debug("2.3 ceHeadFilter --->")
 					out.Write(slMyContract)
 				} else { // 合约不合法
+					beegoLog.Debug("2.3 contract invalid and insert consensusFailure")
 					var consensusFailure model.ConsensusFailure
 					consensusFailure.Id = common.GenerateUUID()
-					consensusFailure.ConsensusType = "contract"
+					consensusFailure.ConsensusType = _CONSENSUSTYPE
 					consensusFailure.ConsensusId = vote.VoteBody.VoteFor
-					consensusFailure.ConsensusReason = "fail contract"
+					consensusFailure.ConsensusReason = _CONSENSUSREASON
 					consensusFailure.Timestamp = common.GenTimestamp()
 
 					slConsensusFailure, err := json.Marshal(consensusFailure)
@@ -157,14 +151,13 @@ func ceHeadFilter(in io.Reader, out io.Writer) {
 
 //---------------------------------------------------------------------------
 func ceQueryEists(in io.Reader, out io.Writer) {
-	log.Printf("3.进入ceQueryEists\n")
-
+	beegoLog.Debug("3.进入ceQueryEists")
 	rd := bufio.NewReader(in)
 	slContractOutput := make([]byte, MaxSizeTX)
 	for {
 		nReadNum, err := rd.Read(slContractOutput)
 		time := monitor.Monitor.NewTiming()
-		log.Printf("读取到数据......\n")
+		beegoLog.Debug("3.1 ceQueryEists get data")
 		if err != nil {
 			beegoLog.Error(err.Error())
 			continue
@@ -180,20 +173,21 @@ func ceQueryEists(in io.Reader, out io.Writer) {
 			beegoLog.Error(err.Error())
 			continue
 		}
-		//log.Printf("19.myContract is %+v\n", myContract)
-		log.Println("19")
 
+		beegoLog.Debug("3.2 query chain")
 		strInput := fmt.Sprintf("{\"id\":\"%s\"}", myContract.Transaction.ContractModel.Id)
 		responseResult, err := chain.GetContract(strInput)
-		//log.Printf("21.responseResult is %+v\n", responseResult)
-		log.Println("21")
 		if err != nil {
 			beegoLog.Error(err.Error())
+			beegoLog.Debug("3.3 query failed and SaveOutputErrorData")
 			SaveOutputErrorData(_TableNameSendFailingRecords, slRealData)
 		} else {
 			if responseResult.Code == _HTTPOK {
-				if responseResult.Data == nil {
+				if responseResult.Data == nil { // not exist
+					beegoLog.Debug("3.3 ceQueryEists --->")
 					out.Write(slRealData)
+				} else { // exist
+					beegoLog.Debug("3.3 exist")
 				}
 			}
 		}
@@ -204,13 +198,13 @@ func ceQueryEists(in io.Reader, out io.Writer) {
 
 //---------------------------------------------------------------------------
 func ceSend(in io.Reader, out io.Writer) {
-	log.Printf("4.进入ceSend\n")
-
+	beegoLog.Debug("4.进入ceSend")
 	rd := bufio.NewReader(in)
 	slContractOutput := make([]byte, MaxSizeTX)
 	for {
 		nReadNum, err := rd.Read(slContractOutput)
 		time := monitor.Monitor.NewTiming()
+		beegoLog.Debug("4.1 ceSend get data")
 		if err != nil {
 			beegoLog.Error(err.Error())
 			continue
@@ -220,19 +214,22 @@ func ceSend(in io.Reader, out io.Writer) {
 		}
 		slRealData := slContractOutput[:nReadNum]
 
+		beegoLog.Debug("4.2 CreateContract")
 		responseResult, err := chain.CreateContract(slRealData)
 		if err != nil {
 			beegoLog.Error(err.Error())
+			beegoLog.Debug("4.3 CreateContract failed and SaveOutputErrorData")
 			SaveOutputErrorData(_TableNameSendFailingRecords, slRealData)
 			continue
 		}
 		if responseResult.Code != _HTTPOK {
 			beegoLog.Error(responseResult.Message)
+			beegoLog.Debug("4.3 CreateContract failed and SaveOutputErrorData")
 			SaveOutputErrorData(_TableNameSendFailingRecords, slRealData)
 		}
 
+		beegoLog.Debug("4.4 ceSend --->")
 		out.Write(slRealData)
-
 		time.Send("ce_send_contract")
 	}
 }
@@ -259,8 +256,6 @@ func startContractElection() {
 // 私有工具函数
 //---------------------------------------------------------------------------
 func _verifyHeadNode(mainPubkey string) (bool, error) {
-	//log.Printf("10.PublicKey  is %+v\n", mainPubkey)
-	log.Println("10")
 	ok := false
 	if mainPubkey == gstrPublicKey {
 		ok = true
@@ -276,23 +271,17 @@ func _verifyVotes(contractId string) ([]byte, bool, error) {
 		return nil, false, err
 	}
 
-	//log.Printf("12.strVotes is %+v\n", strVotes)
-	log.Println("12")
 	var slVote []model.Vote
 	err = json.Unmarshal([]byte(strVotes), &slVote)
 	if err != nil {
 		return nil, false, err
 	}
 
-	//log.Printf("13.slVote is %+v\n", slVote)
-	log.Println("13")
 	nTotalVoteNum := len(slVote)
 	if nTotalVoteNum == 0 {
 		return nil, false, errors.New("no vote")
 	}
 
-	//log.Printf("14.nTotalVoteNum is %+v\n", nTotalVoteNum)
-	log.Println("14")
 	// 验证vote
 	eligible_votes := make(map[string]model.Vote)
 	for _, tmpVote := range slVote {
@@ -304,8 +293,6 @@ func _verifyVotes(contractId string) ([]byte, bool, error) {
 		}
 	}
 
-	//log.Printf("16.eligible_votes is %+v\n", eligible_votes)
-	log.Println("16")
 	// 统计vote并判断valid
 	// do not forget fix debug!!!!
 	//	if len(eligible_votes)*2 < gnPublicKeysNum { // vote没有达到节点数的一半时
@@ -319,9 +306,6 @@ func _verifyVotes(contractId string) ([]byte, bool, error) {
 		if err != nil {
 			return nil, bValid, err
 		}
-
-		//log.Printf("17.contractOutput is %+v\n", contractOutput)
-		log.Println("17")
 		slMyContract, err := json.Marshal(contractOutput)
 		return slMyContract, bValid, err
 	} else {
@@ -332,8 +316,6 @@ func _verifyVotes(contractId string) ([]byte, bool, error) {
 //---------------------------------------------------------------------------
 func _verifyPublicKey(NodePubkey string) (bool, error) {
 	isExist := false
-	//log.Printf("15.publicKeys is %+v\n", publicKeys)
-	log.Println("15")
 	for _, value := range gslPublicKeys {
 		if value == NodePubkey {
 			isExist = true
@@ -379,7 +361,6 @@ func _produceContractOutput(contractId string, slVote []model.Vote) (model.Contr
 	err = json.Unmarshal([]byte(strContract),
 		&contractOutput.Transaction.ContractModel)
 	if err != nil {
-		log.Println(err)
 		return contractOutput, err
 	}
 
