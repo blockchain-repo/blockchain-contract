bd4a89290f35c0c1d8e4a91c5bdb5a8d96756d8f
wangyp@chainen.net <wangyp@chainen.net>
2017-04-27 19:25:11 +0800

    modify scantaskschedule

--- src/pipelines/scantaskschedule.go
+++ src/pipelines/scantaskschedule.go
@@ -2,7 +2,7 @@
 package pipelines
 
 import (
-	"fmt"
+	"encoding/json"
 	"time"
 )
 
@@ -10,22 +10,59 @@ import (
 	beegoLog "github.com/astaxie/beego/logs"
 )
 
-var (
-	_ = 123
+import (
+	"unicontract/src/config"
+	"unicontract/src/core/db/rethinkdb"
+	"unicontract/src/core/model"
 )
 
 const (
-	_TableNameTaskSchedule = "Votes"
-	_SLEEPTIME             = 10
+	_SLEEPTIME = 10
 )
 
 func _ScanTaskSchedule() {
-	beegoLog.Debug("_ScanTaskSchedule")
 	for {
 		start := time.Now()
+		var slTasks []model.TaskSchedule
 
-		//TODO real handle
+		beegoLog.Debug("query database")
+		strNodePubkey := config.Config.Keypair.PublicKey
+		retStr, err := rethinkdb.GetTaskSchedules(strNodePubkey)
+		if err != nil {
+			beegoLog.Error(err.Error())
+			// TODO error handle
+			goto CONSUME
+		}
 
+		beegoLog.Debug("get tasks")
+		err = json.Unmarshal([]byte(retStr), &slTasks)
+		if err != nil {
+			beegoLog.Error(err.Error())
+			// TODO error handle
+			goto CONSUME
+		}
+
+		beegoLog.Debug("handle task")
+		for index, value := range slTasks {
+			slTask, err := json.Marshal(slTasks[index])
+			if err != nil {
+				beegoLog.Error(err.Error())
+				// TODO error handle
+				goto CONSUME
+			}
+
+			beegoLog.Debug("send task")
+			if _SendToList(string(slTask)) {
+				err = rethinkdb.SetTaskScheduleSend(value.Id)
+				if err != nil {
+					beegoLog.Error(err.Error())
+					// TODO error handle
+					goto CONSUME
+				}
+			}
+		}
+
+	CONSUME:
 		consume := time.Since(start)
 		if consume < _SLEEPTIME {
 			time.Sleep((_SLEEPTIME - consume) * time.Second)
@@ -38,6 +75,5 @@ func _SendToList(strTaskSchedule string) bool {
 }
 
 func startScanTaskSchedule() {
-	fmt.Println("startScanTaskSchedule")
 	go _ScanTaskSchedule()
 }
