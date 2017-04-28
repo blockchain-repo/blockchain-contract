5eec90575a5b2e19d0de08d466d891d22f4540f2
wangxin <wx_cs_db_88@163.com>
2017-04-28 18:33:16 +0800

    add the script for generating js java files [use *.proto files]

--- protoc-tools.sh
+++ protoc-tools.sh
@@ -7,7 +7,14 @@ protos_file_path=${current_path}/src/core/protos
 # remove the string <,omitempty> in generated .pb.go files[proto3]
 remove_omitempty=true
 
-proto_support_array=("go","java","js","python")
+#proto_support_array=("go","java","js","python")
+proto_support_array=("go","java","js")
+
+proto_js_libs_name="proto_contract_libs"
+
+if [[ $# -le 0 ]]; then
+    echo -e "input args should in ${proto_support_array}"
+fi
 
 function generateProto ()
 {
@@ -24,7 +31,15 @@ function generateProto ()
 
         echo "proto-gen-$@"
         echo -e "reproduce the *.go files according to the ${protos_file_path}/*.proto files"
-        protoc -I=${protos_file_path} --"$@"_out=${outputDir} ${protos_file_path}/*.proto
+        if [ "$@" == "js" ]; then
+#            protoc -I=${protos_file_path} --js_out=library=${proto_js_libs_name},binary:${outputDir} contract.proto
+
+#            protoc --js_out=library=${proto_js_libs_name},binary:${outputDir} ${protos_file_path}/*.proto
+            echo -e "protoc --js_out=library=${proto_js_libs_name},binary:${outputDir} ${protos_file_path}/*.proto"
+            protoc -I=${protos_file_path} --js_out=library=${proto_js_libs_name},binary:${outputDir} ${protos_file_path}/*.proto
+        else
+            protoc -I=${protos_file_path} --"$@"_out=${outputDir} ${protos_file_path}/*.proto
+        fi
         echo -e "reproduce success!"
 
         if [ "$@" == "go" ] && [ ${remove_omitempty} == true ]; then
@@ -32,7 +47,6 @@ function generateProto ()
             sed  -i 's/,omitempty//g' ${protos_file_path}/*.pb.go
             echo -e "replace the <,omitempty> success!"
         fi
-
     fi
 }
 
