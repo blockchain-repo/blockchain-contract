db9b0e34c1be5a2254a335ec36dae4e425beb4af
wangxin <wx_cs_db_88@163.com>
2017-05-27 17:36:08 +0800

    fixed the contrac.proto field modify

--- src/core/core.go
+++ src/core/core.go
@@ -4,10 +4,10 @@ import (
 	"math/rand"
 	"time"
 
+	"unicontract/src/common"
 	"unicontract/src/config"
 	r "unicontract/src/core/db/rethinkdb"
 	"unicontract/src/core/model"
-	"unicontract/src/common"
 )
 
 func WriteContract(contract model.ContractModel) bool {
@@ -15,7 +15,7 @@ func WriteContract(contract model.ContractModel) bool {
 	pubs := config.GetAllPublicKey()
 
 	contract.ContractHead.MainPubkey = pubs[rand.Intn(len(pubs))]
-	contract.ContractHead.Timestamp = common.GenTimestamp()
+	contract.ContractHead.OperateTime = common.GenTimestamp()
 	ok := r.InsertContract(contract.ToString())
 	return ok
 }
