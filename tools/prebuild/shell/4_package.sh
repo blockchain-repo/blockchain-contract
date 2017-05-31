b216b4d034426193abb8d406a4cb7d1934b3afce
wangyp@chainen.net <wangyp@chainen.net>
2017-05-31 16:16:13 +0800

    modify shell

--- tools/prebuild/shell/4_package.sh
+++ tools/prebuild/shell/4_package.sh
@@ -8,14 +8,23 @@ package_name="package.tar.gz"
 
 # "----------------------------------------------"
 cd $current_path
-mkdir package
-cp -R bin package
-cp -R conf package
-cp -R data package
-cp -R log package
+if [ -d "package" ]
+then
+    rm -rf "package"
+fi
+mkdir "package"
+mkdir "package/log"
+mkdir "package/data"
+cp -R "bin" "package"
+cp -R "conf" "package"
 
 cd $current_path
-tar -zcvf $package_name ./package
+if [ -f $package_name ]
+then
+    rm -f $package_name
+fi
+tar -zcvf $package_name "package"
+rm -rf "package"
 # "----------------------------------------------"
 
 # "----------------------------------------------"
