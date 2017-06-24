f8da9902f4fefe9cb230daca53ee84bd9052e91a
wangyp@chainen.net <wangyp@chainen.net>
2017-06-24 15:45:25 +0800

    modify

--- src/api/routers/router.go
+++ src/api/routers/router.go
@@ -9,11 +9,18 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	"unicontract/src/api/controllers"
 	"unicontract/src/api/filters"
 )
 
 func init() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+		AllowCredentials: true}))
 
 	beego.InsertFilter("/*", beego.BeforeRouter, filters.ContractFilter, false)
 	beego.InsertFilter("/*", beego.BeforeRouter, filters.MonitorFilter, false)
